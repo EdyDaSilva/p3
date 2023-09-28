9ef81bc327825a57107edb6d6742e7ad924374b1
EdyDaSilva <ednedson@hotmail.com>
2023-09-28 12:19:20 +0100

    first commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import csv
+
+with open("sheep_data.csv", "w+") as file:
+    myFile = csv.writer(file)
+    myFile.writerow(["ID Tag", "DOB", "Sex", "Other Info"])
