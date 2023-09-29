b70ff2a9e2a64b9ff46746efcd7c8395d5c484d7
EdyDaSilva <ednedson@hotmail.com>
2023-09-29 09:17:27 +0100

    re expression to date format while enter DOB

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 # create csv file
 # with open("sheep_data.csv", "w+") as file:
@@ -9,7 +10,8 @@ def append_to_csv(filename):
     # Open the CSV file in append mode
     with open("sheep_data.csv", "a", newline="") as file:
         myFile = csv.writer(file)
-        
+
+        # id_tag = int(input("Enter ID Tag: "))
         # Prompt the user for information
         while True:
             id_tag = input("Enter ID Tag: ")
@@ -19,8 +21,16 @@ def append_to_csv(filename):
                 break
             else:
                 print("Please enter a valid number for ID Tag.")
-        # id_tag = int(input("Enter ID Tag: "))
-        dob = input("Enter DOB: ")
+        
+        while True:
+            dob = input("Enter DOB (YYYY-MM-DD): ")
+            # Use regular expression to check if input matches the date format YYYY-MM-DD
+            if re.match(r'^\d{4}-\d{2}-\d{2}$', dob):
+                break
+            else:
+                print("Please enter a valid date in the format YYYY-MM-DD.")
+        
+        # dob = input("Enter DOB: ")
         sex = input("Enter Sex: ")
         other_info = input("Enter Other Info: ")
         
